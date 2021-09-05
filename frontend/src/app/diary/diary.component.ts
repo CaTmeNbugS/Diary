2271b038cc99e5250867fccfff14859f012ac143
CaTmeNbugS <catmen826@gmail.com>
2021-09-05 13:03:26 +0500

    0.0.2

---
+++ frontend/src/app/diary/diary.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'diary',
+  templateUrl: './diary.component.html',
+  styleUrls: ['./diary.component.css'],
+})
+export class DiaryComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
